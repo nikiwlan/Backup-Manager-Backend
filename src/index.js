7b829b8110d32c217f6af26d3d86be09234ef1a5
Erik Cichy <erik.cichy@hs-weingarten.de>
2022-11-25 19:55:04 +0100

    ssh2 prototype (password still throws error)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,61 @@
-const express = require("express");
-const app = express();
+// SOURCE:
+// https://stackoverflow.com/questions/50372083/create-a-node-ssh2-server-with-ability-to-treat-remote-forwarding
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hello World</h1>");
-});
+var fs = require('fs');
+// var crypto = require('crypto');
+var inspect = require('util').inspect;
 
+//var buffersEqual = require('buffer-equal-constant-time');
+var ssh2 = require('ssh2');
+//var utils = ssh2.utils;
 
-app.listen(5000, () => {
-    console.log("server started on port 5000");
+new ssh2.Server({
+    hostKeys: [fs.readFileSync('key/id_rsa')]
+}, function (client) {
+    console.log('Client connected!');
+
+    client.on('authentication', function (ctx) {
+        if (ctx.method === 'password' &&
+            ctx.username === 'foo' &&
+            ctx.password === 'bar')
+            ctx.accept();
+        else
+            ctx.reject();
+    }).on('ready', function () {
+        console.log('Client authenticated!');
+
+        client.on('session', function (accept, reject) {
+            var session = accept();
+            session.once('exec', function (accept, reject, info) {
+                console.log('Client wants to execute: ' + inspect(info.command));
+                var stream = accept();
+                stream.stderr.write('Oh no, the dreaded errors!\n');
+                stream.write('Just kidding about the errors!\n');
+                stream.exit(0);
+                stream.end();
+            });
+        });
+        client.on('request', function (accept, reject, name, info) {
+            console.log(info);
+            if (name === 'tcpip-forward') {
+                accept();
+                setTimeout(function () {
+                    console.log('Sending incoming tcpip forward');
+                    client.forwardIn(info.bindAddr,
+                        info.bindPort,
+                        function (err, stream) {
+                            if (err)
+                                return;
+                            stream.end('hello world\n');
+                        });
+                }, 1000);
+            } else {
+                reject();
+            }
+        });
+    });
+}).listen(5000, 'localhost', function () {
+    // Connect via following command in cmd:
+    // ssh foo@localhost -p 5000
+    console.log('Listening on port ' + this.address().port);
 });
