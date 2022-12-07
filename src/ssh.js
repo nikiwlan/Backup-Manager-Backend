ea056a41e0cffd61972415d226dd2d58939ba611
Erik Cichy <erik.cichy@hs-weingarten.de>
2022-12-07 21:17:39 +0100

    added ssh2-sftp-client

--- src/ssh.js
+++ src/ssh.js
@@ -29,6 +29,7 @@ conn.on('ready', () => {
 
 
 // TRANSMIT FILE VIA SFTP
+// not recommended ---> ssh2-sftp-client is easier (see below)
 conn.on('ready', () => {
   console.log('Client :: ready');
   conn.sftp(function (err, sftp) {
@@ -65,3 +66,46 @@ conn.on('ready', () => {
   username: 'ssh_server',
 });
 
+// ==============================================================================
+
+// https://github.com/theophilusx/ssh2-sftp-client
+
+let sftpClient = require('ssh2-sftp-client');
+let sftp = new sftpClient();
+
+let data_ = 'write.json';
+let data = fs.createReadStream(data_);
+let remote_write = 'some_folder/write.json';
+
+let remote_read = 'some_folder/read.json';
+let dst_ = 'read.json';
+let dst = fs.createWriteStream(dst_);
+
+sftp.connect({
+  host: 'localhost',
+  username: 'ssh_server',
+  password: 'admin'
+}).then(() => {
+  // create a file on SSH Server
+  return sftp.put(data, remote_write);
+}).then(() => {
+  // Uploads the data in file at data_ to a new file on remote server at remote_write using concurrency.
+  return sftp.fastPut(data_, remote_write);
+}).then(() => {
+  // get a file from SSH Server
+  return sftp.get(remote_read, dst);
+}).then(() => {
+  // Downloads a file at remote_read to dst_ using parallel reads for faster throughput.
+  return sftp.fastGet(remote_read, dst_);
+}).then(() => {
+  return sftp.end();
+}).catch(err => {
+  console.error(err.message);
+});
+
+// other functions:
+// sftp.mkdir(directory_path, true);
+// sftp.rmdir(directory_path, true);
+// sftp.delete(file);
+// sftp.rename(file);
+// sftp.chmod(file, newMode);
