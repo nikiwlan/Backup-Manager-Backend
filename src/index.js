b1e51f03f9370c5ef180b928b1d843e3249cf273
Niklas Langes <niklas.langes@hs-weingarten.de>
2022-12-13 17:10:13 +0100

    pw change impl.

--- src/index.js
+++ src/index.js
@@ -1,166 +1,186 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-var fs = require('fs');
-const formidable = require('express-formidable');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+var fs = require("fs");
+const formidable = require("express-formidable");
 const AdmZip = require("adm-zip");
-const saltRounds = 10
-const password = "Admin@123"
+const saltRounds = 10;
+const password = "Admin@123";
 var zipfile_num = 0;
 
-function createPassword(passowrd_to_hash)
-{
-    return bcrypt.hashSync(passowrd_to_hash, 5);
+function createPassword(passowrd_to_hash) {
+  return bcrypt.hashSync(passowrd_to_hash, 5);
 }
 
 const path = "D:/test_backup";
 
-function getDirectories(current_path)
-{
+function getDirectories(current_path) {
   var file_list = fs.readdirSync(current_path, { withFileTypes: true });
-  var return_file_list = []
-  file_list.forEach(function(x,i) {
-    if (x.isDirectory())return_file_list.push({name: x["name"], directory: x.isDirectory()})
-  } ) 
-  file_list.forEach(function(x,i) {
-    if (!x.isDirectory())return_file_list.push({name: x["name"], directory: x.isDirectory()})
-  } ) 
+  var return_file_list = [];
+  file_list.forEach(function (x, i) {
+    if (x.isDirectory())
+      return_file_list.push({ name: x["name"], directory: x.isDirectory() });
+  });
+  file_list.forEach(function (x, i) {
+    if (!x.isDirectory())
+      return_file_list.push({ name: x["name"], directory: x.isDirectory() });
+  });
   return return_file_list;
 }
-
 
 const app = express();
 app.use(cors());
 app.use(formidable());
-var data = JSON.parse(fs.readFileSync( "./data.json", {encoding:'utf8', flag:'r'}))
-console.log(data)
-function checkPassword(passowrd_to_check, hash)
-{
-  console.log(passowrd_to_check, hash)
-    return bcrypt.compareSync(passowrd_to_check, hash);
+var data = JSON.parse(
+  fs.readFileSync("./data.json", { encoding: "utf8", flag: "r" })
+);
+console.log(data);
+
+function set_new_password(username, password, new_password) {
+  if (checkUser(username, password, data["users"])) {
+    data["users"][username] = createPassword(new_password);
+    fs.writeFile(
+      "./data.json",
+      JSON.stringify(data),
+      function (err) {
+        if (err) {
+          console.log(err);
+          return 0;
+        }
+      }
+    );
+    return 1;
+  }
+  return 0;
 }
 
-function checkUser(username, password, users)
-{
-    // -1 user not found, 0 wrong password, 1 password fits username
-    if  (!(username in users)) return -1;
-    console.log(password, users[username])
-    if (checkPassword(password, users[username])) return 1;
-    return -1;
+function checkPassword(passowrd_to_check, hash) {
+  console.log(passowrd_to_check, hash);
+  return bcrypt.compareSync(passowrd_to_check, hash);
 }
 
-app.post('/login', (req, res, next) => {
-    var status = checkUser(req.fields.username, req.fields.password, data["users"])
-    console.log(status);
-    if (status == -1) return res.status(401).json({
-        title: 'invalid username or ',
-        error: 'invalid credentials'
-      })
-      console.log("ok");
-      let token = jwt.sign({ userId: req.fields.username}, 'secretkey');
-      return res.status(200).json({
-        title: 'login sucess',
-        token: token
-      })
+function checkUser(username, password, users) {
+  // -1 user not found, 0 wrong password, 1 password fits username
+  if (!(username in users)) return -1;
+  console.log(password, users[username]);
+  if (checkPassword(password, users[username])) return 1;
+  return -1;
+}
 
+app.post("/login", (req, res, next) => {
+  var status = checkUser(
+    req.fields.username,
+    req.fields.password,
+    data["users"]
+  );
+  console.log(status);
+  if (status == -1)
+    return res.status(401).json({
+      title: "invalid username or ",
+      error: "invalid credentials",
+    });
+  console.log("ok");
+  let token = jwt.sign({ userId: req.fields.username }, "secretkey");
+  return res.status(200).json({
+    title: "login sucess",
+    token: token,
+  });
+});
 
-})
-
-console.log("He")
-app.post('/fileexplorer', (req, res, next) => {
+console.log("He");
+app.post("/fileexplorer", (req, res, next) => {
   var current_path = path + req.fields.directory;
   console.log(current_path);
-  if (!fs.existsSync(current_path))
-  {
-    console.log("failed Dir")
+  if (!fs.existsSync(current_path)) {
+    console.log("failed Dir");
     return res.status(401).json({
-          title: 'path does not exist ',
-          error: 'invalid credentials'
-        })
+      title: "path does not exist ",
+      error: "invalid credentials",
+    });
   }
   var file_list = getDirectories(current_path);
   res.status(200).send(file_list);
-})
+});
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, (err) => {
   if (err) return console.log(err);
-  console.log('server running on port ' + port);
-})
+  console.log("server running on port " + port);
+});
 
-app.post("/upload",(req, res, next) => {
-
-  for (const [key, value] of Object.entries(req.files)){
-    
-    var rawData = fs.readFileSync(value.path)
-    var new_path = path + req.fields.destination  +value.name;
-    fs.writeFile(new_path, rawData, function(err){
-      if(err) {return res.status(401).json({
-        title: 'Failed to upload '
-      }); return;}
-      
-  })
+app.post("/upload", (req, res, next) => {
+  for (const [key, value] of Object.entries(req.files)) {
+    var rawData = fs.readFileSync(value.path);
+    var new_path = path + req.fields.destination + value.name;
+    fs.writeFile(new_path, rawData, function (err) {
+      if (err) {
+        return res.status(401).json({
+          title: "Failed to upload ",
+        });
+        return;
+      }
+    });
   }
   res.status(200).send("OK");
-} );
+});
 
-app.get("/download",(req, res, next) => {
+app.get("/download", (req, res, next) => {
   zipfile_num += 1;
-  const filepath = path + req.query["file_path"] ;
-  
+  const filepath = path + req.query["file_path"];
+
   output_file_name = "/target" + zipfile_num + ".zip";
   const zip = new AdmZip();
-  if (fs.lstatSync(path + req.query["file_path"]).isDirectory()){
+  if (fs.lstatSync(path + req.query["file_path"]).isDirectory()) {
     zip.addLocalFolder(filepath);
+  } else {
+    zip.addLocalFile(filepath);
   }
-  else {
-    zip.addLocalFile(filepath);  }
-  
-    zip.writeZip(__dirname +output_file_name);
 
+  zip.writeZip(__dirname + output_file_name);
 
-  res.download(__dirname +output_file_name);
+  res.download(__dirname + output_file_name);
+});
 
-} )
-
-app.post("/remove_file",(req, res, next) => {
-  rm_file = path +req.fields.path_to_delete;
+app.post("/remove_file", (req, res, next) => {
+  rm_file = path + req.fields.path_to_delete;
   if (fs.existsSync(rm_file)) fs.unlinkSync(rm_file);
-  else  return res.status(401).json({
-    title: 'failed ',
-    error: 'failed'
-  })
+  else
+    return res.status(401).json({
+      title: "failed ",
+      error: "failed",
+    });
 
   if (!fs.existsSync(rm_file)) res.status(200).send("OK");
-  else return res.status(401).json({
-    title: 'failed ',
-    error: 'failed'
-  })
-} )
+  else
+    return res.status(401).json({
+      title: "failed ",
+      error: "failed",
+    });
+});
 
-app.post("/move_file",(req, res, next) => {
- var source_file = path + req.fields.path_to_move;
- var dest_dir = path + req.fields.dest_dir;
- fs.renameSync(source_file, dest_dir, function (err) {
-  if (err) return res.status(401).json({
-    title: 'failed ',
-    error: 'failed'
-  })
-})
-res.status(200).send("OK");
-} )
+app.post("/move_file", (req, res, next) => {
+  var source_file = path + req.fields.path_to_move;
+  var dest_dir = path + req.fields.dest_dir;
+  fs.renameSync(source_file, dest_dir, function (err) {
+    if (err)
+      return res.status(401).json({
+        title: "failed ",
+        error: "failed",
+      });
+  });
+  res.status(200).send("OK");
+});
 
-
-app.post("/changePassword",(req, res, next) => {
-  if(req.fields.oldPassword  === password ){
-     //password = req.fields.oldPassword;
-     res.status(200).send("OK");
+app.post("/changePassword", (req, res, next) => {
+  if (set_new_password("amin", req.fields.oldPassword, String(req.fields.newPassword))){
+    res.status(200).send("OK");
   }
-  else{
-    res.status(401).send("failed")
+  else {
+    res.status(401).send("failed");
   }
- })
+});
+
 
 
