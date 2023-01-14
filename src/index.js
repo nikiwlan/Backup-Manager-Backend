0096440a203218ea53822237b14d9f0fd0525168
ms-191045 <maximilian.sigg@hs-weingarten.de>
2023-01-14 01:32:03 +0100

    diffie hellman

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ const path_os = require('path');
 let sftpClient = require('ssh2-sftp-client');
 const { exec } = require("child_process");
 const schedule = require('node-schedule');
+const CryptoJS = require('crypto-js');
+
+
+console.log("He");
 /*
 var ssh_config = {
   host: 'localhost',
@@ -18,9 +22,12 @@ var ssh_config = {
   password: 'admin'
 };
 */
-const { generateKeyPair } = require('crypto');
+const { generateKeyPair, randomInt } = require('crypto');
+
 var zipfile_num = 0;
 let access_tokens = []
+
+let user_encrypt = {}
 function createPassword(passowrd_to_hash)
 {
     return bcrypt.hashSync(passowrd_to_hash, 5);
@@ -32,6 +39,9 @@ function createFolder(path_folder){
   
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 const corsOptions = {origin: ["http://192.168.0.107:8080", "http://localhost:8080"], optionsSuccessStatus: 200}
 
@@ -92,6 +102,7 @@ function checkPassword(passowrd_to_check, hash)
     return bcrypt.compareSync(passowrd_to_check, hash);
 }
 
+
 function checkUser(username, password, users)
 {
     // -1 user not found, 0 wrong password, 1 password fits username
@@ -101,16 +112,35 @@ function checkUser(username, password, users)
     return -1;
 }
 
+
+function setSecret(username){
+  user_encrypt[username]["test_val"] = getRandomInt(100000000).toString();
+  console.log(user_encrypt[username]["test_val"] );
+  return CryptoJS.AES.encrypt(user_encrypt[username]["test_val"] ,user_encrypt[username]["ky"]).toString();  
+      
+}
+
+function check_valid(req){
+  if (!(req.fields.username in user_encrypt)) return false;
+  if (!(req.fields.username + user_encrypt[req.fields.username]["test_val"] == CryptoJS.AES.decrypt(req.fields.secret,user_encrypt[req.fields.username]["ky"]).toString(CryptoJS.enc.Utf8) )) return false;
+  //console.log(CryptoJS.AES.decrypt(req.fields.secret,user_encrypt[req.fields.username]["ky"]).toString(CryptoJS.enc.Utf8) );
+  return true;
+}
+
 app.post('/login', (req, res, next) => {
 
-    var status = checkUser(req.fields.username, req.fields.password, data["users"])
+
+  let t = CryptoJS.AES.decrypt(req.fields.password.toString(),user_encrypt[req.fields.username]["ky"]).toString(CryptoJS.enc.Utf8);
+  
+  var status = checkUser(req.fields.username, t, data["users"])
     console.log(status);
     if (status == -1) return;
       console.log("ok");
       let token = jwt.sign({ userId: req.fields.username}, 'secretkey');
       return res.status(200).json({
         title: 'login sucess',
-        token: token
+        token: token,
+        secret: setSecret(req.fields.username)
       })
 
 
@@ -150,6 +180,10 @@ app.post('/sshcheck', (req, res, next) => {
     title: 'Failed',
     error: 'Failed'
   })
+  if (!check_valid(req))return res.status(401).json({
+    title: 'Failed',
+    error: 'Failed'
+  })
   let sftp = new sftpClient();
   var ssh_config = {
     host: req.fields.host,
@@ -168,7 +202,9 @@ app.post('/sshcheck', (req, res, next) => {
   })
   .then(() => {
     sftp.end();
-    res.status(200).send("Ok");
+    return res.status(200).json({
+      secret:setSecret(req.fields.username)
+   })
   })
   .catch(err => {
     console.error(err.message);
@@ -203,7 +239,9 @@ app.post('/mkdir', (req, res, next) => {
     }}
     )
 
-    res.status(200).send("OK");
+    return res.status(200).json({
+      secret:setSecret(req.fields.username)
+   })
 
 
 })
@@ -255,6 +293,10 @@ function createPath(original_path, path_list){
 const port = process.env.PORT || 5000;
 
 app.post("/upload",(req, res, next) => {
+  if (!check_valid(req))return res.status(401).json({
+    title: 'Failed',
+    error: 'Failed'
+  })
   // Token verifizieren
   if (!verify_token(req.fields.auth)) return res.status(401).json({
     title: 'Failed',
@@ -283,10 +325,22 @@ app.post("/upload",(req, res, next) => {
       
   })
   }
-  res.status(200).send("OK");
+  return res.status(200).json({
+    secret:setSecret(req.fields.username)
+ })
 } );
 
+function check_valid2(username, secret){
+  if (!(username in user_encrypt)) return false;
+  if (!(username + user_encrypt[username]["test_val"] == CryptoJS.AES.decrypt(secret,user_encrypt[username]["ky"]).toString(CryptoJS.enc.Utf8) )) return false;
+  //console.log(CryptoJS.AES.decrypt(req.fields.secret,user_encrypt[req.fields.username]["ky"]).toString(CryptoJS.enc.Utf8) );
+  return true;
+}
 app.get("/download",(req, res, next) => {
+  if (!check_valid2(req.query.username, req.query.secret))return res.status(401).json({
+    title: 'Failed',
+    error: 'Failed'
+  })
   if (!verify_token(req.query.auth)) return res.status(401).json({
     title: 'Failed',
     error: 'Failed'
@@ -316,12 +370,17 @@ app.get("/download",(req, res, next) => {
   
     zip.writeZip(path_os.join(__dirname,output_file_name));
 
-
+    user_encrypt[req.query.username]["test_val"] += "1";
+  
   res.download(path_os.join(__dirname,output_file_name));
 
 } )
 
 app.post("/remove_file",(req, res, next) => {
+  if (!check_valid(req))return res.status(401).json({
+    title: 'Failed',
+    error: 'Failed'
+  })
   // rm_file = path +req.fields.path_to_delete;
   if (!verify_token(req.fields.auth)) return res.status(401).json({
     title: 'Failed',
@@ -341,7 +400,9 @@ app.post("/remove_file",(req, res, next) => {
     error: 'failed'
   })
 
-  if (!fs.existsSync(rm_file)) res.status(200).send("OK");
+  if (!fs.existsSync(rm_file)) return res.status(200).json({
+    secret:setSecret(req.fields.username)
+ })
   else return res.status(401).json({
     title: 'failed ',
     error: 'failed'
@@ -365,6 +426,10 @@ function set_new_password(username, password, new_password) {
 
 
 app.post('/fileexplorer', (req, res, next) => {
+  if (!check_valid(req))return res.status(401).json({
+    title: 'Failed',
+    error: 'Failed'
+  })
 
   if (!verify_token(req.fields.auth)) res.status(401).json({
     title: 'Failed',
@@ -388,7 +453,10 @@ app.post('/fileexplorer', (req, res, next) => {
     })
   }
   var file_list = getDirectories(current_path);
-  res.status(200).send(file_list);
+  return res.status(200).json({
+    secret:setSecret(req.fields.username),
+    file_list: file_list
+ })
 })
 
 function getbackup()
@@ -396,12 +464,19 @@ function getbackup()
   return data["backup"]
 }
 app.post('/getbackupserver', (req, res, next) => {
+  if (!check_valid(req))return res.status(401).json({
+    title: 'Failed',
+    error: 'Failed'
+  })
 
   if (!verify_token(req.fields.auth)) return res.status(401).json({
     title: 'Failed',
     error: 'Failed'
   })
-  res.status(200).send(getbackup())
+  return res.status(200).json({
+    secret:setSecret(req.fields.username),
+    backup: getbackup()
+ })
 })
 
 function rmServer(serverAddress) {
@@ -427,13 +502,19 @@ fs.writeFile(data_path, JSON.stringify(data), function (err) {
   }}
 
 app.post('/removeserver', (req, res, next) => {
+  if (!check_valid(req))return res.status(401).json({
+    title: 'Failed',
+    error: 'Failed'
+  })
 
   if (!verify_token(req.fields.auth)) return res.status(401).json({
     title: 'Failed',
     error: 'Failed'
   })
   rmServer(req.fields.rmserver);
-  res.status(200).send("Ok")
+  return res.status(200).json({
+     secret:setSecret(req.fields.username)
+  })
 })
 
 app.post("/changePassword", (req, res, next) => {
@@ -446,7 +527,134 @@ app.post("/changePassword", (req, res, next) => {
 
 
 
+
+function isPrime(num){
+  for(let i = 2, s = Math.sqrt(num); i <= s; i++) {
+      if(num % i === 0) return false;
+  }
+  return num > 1;
+}
+
+function getPrim(pMax){
+  prime = getRandomInt(pMax);
+  while(!isPrime(prime))
+  {
+    prime = getRandomInt(pMax);
+  }
+  return prime;
+
+}
+
+function primeFactors(n) {
+  const factors = [];
+  let divisor = 2;
+
+  while (n >= 2) {
+    if (n % divisor == 0) {
+      factors.push(divisor);
+      n = n / divisor;
+    } else {
+      divisor++;
+    }
+  }
+  return factors;
+}
+
+
+
+
+function power(x, n, p){
+  res = 1;
+  for (let i = 1; i <= n; i++) res = (res * x) % p;
+  return res;
+}
+function get_diffie()
+{
+  let p = getPrim(10000000);
+  let prim_factors = primeFactors(p -1);
+  let g = -1
+  for(let r = 3; r <= p-1; r++){
+    let flag = false;
+    for(let i= 0; i < prim_factors.length; i++){
+        if (power(r, Math.floor(p-1 / i), p)){
+            flag = true;
+            g = r;
+            break;
+        }
+    }
+    if (flag) {g = r; break;}
+  }
+  let a  = getRandomInt(p-1);
+  return [p, g, a, power(g,a,p)]
+}
+
+app.post('/login_name', (req, res, next) => {
+  if (!(req.fields.username in data["users"])) return res.status(401).json({
+    failed: "Failed"
+  })
+  let repeat = true;
+  let val = []
+  while (repeat){
+    try {
+      repeat = false;
+      val = get_diffie();
+    } catch (error) {
+      repeat = true;
+    }
+  }
+  
+  dict_val = {"P": val[0], "G": val[1], "Y": val[3], "y": val[2] }
+  user_encrypt[req.fields.username] =  dict_val;
+  return res.status(200).json({
+    title: 'name ok',
+    Y: dict_val["Y"],
+    P: dict_val["P"],
+    G: dict_val["G"]
+  })
+
+
+  /*
+  let name = data["users"];
+  let p = getPrim(10000000);
+
+  console.log("He");
+  
+  let g = 
+  user_encrypt[name] = 
+  var status = checkUser(req.fields.username, req.fields.password, data["users"])
+  console.log(status);
+  if (status == -1) return;
+    console.log("ok");
+    let token = jwt.sign({ userId: req.fields.username}, 'secretkey');
+    return res.status(200).json({
+      title: 'login sucess',
+      token: token
+    })
+    */
+})
+
+app.post('/key', (req, res, next) => {
+  if (!(req.fields.username in user_encrypt)) return res.status(401).json({
+    failed: "Failed"
+  })
+  if (("X" in user_encrypt[req.fields.username])) return res.status(401).json({
+    failed: "Failed"
+  })
+
+
+  user_encrypt[req.fields.username]["X"] =  req.fields.X *1;
+  user_encrypt[req.fields.username]["ky"] = power(req.fields.X, user_encrypt[req.fields.username]["y"], user_encrypt[req.fields.username]["P"] ).toString();
+  return res.status(200).json({
+    title: 'name ok',
+  })
+
+})
+
+//console.log("He");
+
 app.listen(port, (err) => {
   if (err) return console.log(err);
   console.log('server running on port ' + port);
 })
+
+
